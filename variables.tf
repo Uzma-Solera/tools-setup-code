87b7e4ca903ed1787e4a8cd656b08209094dd2ea
uzmaa <uzmaashfaq.shavsi@gmail.com>
2025-04-06 22:46:59 +0530

    This is my first terraform file.

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "ami_id" {
-  default = ami-09c813fb71547fc4f
+  default = "ami-09c813fb71547fc4f"
 }
 
 
 variable "zone_id" {
-  default = Z01537493BA6YJ34JEG5T
+  default = "Z01537493BA6YJ34JEG5T"
 }
 
 variable "tools" {
